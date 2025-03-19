7c287445c1c074466808634d69fc7a43c94dc067
OndraMarek <czondrejmarek@gmail.com>
2025-03-19 22:46:05 +0100

    added prefix_sums_divide_and_conquer project

---
+++ prefix_sums_divide_and_conquer/Program.cs
@@ -0,0 +1,66 @@
+﻿class PrefixSumsDivideAndConquer
+{
+    static void Main()
+    {
+        int[] A = getUserInput();
+        int[] S = ComputePrefixSumsDivideAndConquer(A);
+
+        printOutput(A, S);
+
+        exitOrContinue();
+    }
+
+    public static int[] ComputePrefixSumsDivideAndConquer(int[] A)
+    {
+        int n = A.Length;
+        int[] S = new int[n];
+
+        return S;
+    }
+
+    private static int[] getUserInput()
+    {
+        Console.WriteLine("Enter the elements separated by SPACES (number of elements must be a power of 2): ");
+        string[] input = Console.ReadLine().Split(' ');
+        int n = input.Length;
+
+        if ((n & (n - 1)) != 0)
+        {
+            Console.WriteLine("The number of elements must be a power of 2.");
+            return getUserInput();
+        }
+
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            if (!int.TryParse(input[i], out A[i]))
+            {
+                Console.WriteLine($"Invalid input '{input[i]}'. Please enter only integers.");
+                return getUserInput();
+            }
+        }
+        return A;
+    }
+
+    private static void printOutput(int[] A, int[] S)
+    {
+        Console.WriteLine("Input array:");
+        Console.WriteLine(string.Join(", ", A));
+        Console.WriteLine("Prefix sums:");
+        Console.WriteLine(string.Join(", ", S));
+    }
+
+    private static void exitOrContinue()
+    {
+        Console.WriteLine("Press 1 to continue or any other key to exit.");
+        ConsoleKeyInfo key = Console.ReadKey(true);
+        if (key.KeyChar == '1')
+        {
+            Main();
+        }
+        else
+        {
+            Environment.Exit(0);
+        }
+    }
+}
